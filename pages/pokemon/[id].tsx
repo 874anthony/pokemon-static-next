a1ecc3094c2793102321a4bdad5d74b266f2af56
Anthony Acosta M <anthony082008@hotmail.es>
2022-07-10 17:47:42 -0500

    App finished ISR e ISG

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -87,17 +87,28 @@ export const getStaticPaths: GetStaticPaths = async (ctx) => {
     paths: pokemonsList.map((id) => ({
       params: { id },
     })),
-    fallback: false,
+    fallback: 'blocking',
   };
 };
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   const { id } = params as { id: string };
+  const pokemon = await getPokemonInfo(id);
+
+  if (!pokemon) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    };
+  }
 
   return {
     props: {
-      pokemon: await getPokemonInfo(id),
+      pokemon,
     },
+    // revalidate: 86400, // 60 * 60 * 24 = 1 day
   };
 };
 
